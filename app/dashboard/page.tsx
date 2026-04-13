67d4a8a8afa03919a969af472624d797ca01d793
Diogo Henrique <diogoh.o.92@gmail.com>
2026-04-13 11:31:00 +0100

    tratando erro

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -49,11 +49,13 @@ useEffect(() => {
     fetchMyLeaves()
   }, [])
 
-  if (!mounted) return null
-
   useEffect(() => {
   fetchUser()
 }, [])
+
+  if (!mounted) return null
+
+  
 
 const fetchUser = async () => {
   const res = await fetch("/api/me")
