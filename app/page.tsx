75382c86b03910fda2631657d2b8c95c5f07d253
Diogo Henrique <diogoh.o.92@gmail.com>
2026-07-18 13:25:44 +0100

    logo dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
     <div
@@ -17,13 +18,32 @@ export default function Home() {
         style={{
           textAlign: "center",
           padding: 40,
-          borderRadius: 12,
+          borderRadius: 16,
           background: "#1e293b",
           boxShadow: "0 10px 30px rgba(0,0,0,0.5)",
-          width: 350,
+          width: 380,
         }}
       >
-        <h1 style={{ marginBottom: 10 }}>🚀 Leaves Request</h1>
+        {/* Logo */}
+        <div
+        style={{
+          background: "#ffffff",
+          borderRadius: 12,
+          padding: "12px 20px",
+          display: "inline-block",
+          marginBottom: 20,
+        }}
+        >
+          <Image
+          src="/Bidvest-noonan.jpg"
+          alt="Bidvest Noonan"
+          width={220}
+          height={70}
+          priority
+          />
+        </div>
+
+        <h2 style={{ marginBottom: 10 }}>🚀 Leaves Request</h2>
 
         <p style={{ color: "#94a3b8", marginBottom: 30 }}>
           Bidvest Noonan Vaction Management
